63ffbb4fd759db33d6248df3f61d21669aa9356c
Joanna <joanna.heldak@gmail.com>
2023-10-24 13:36:08 +0200

    message

--- task.py
+++ task.py
@@ -1,26 +1,44 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
 # - be able to create new banks
 # - store client information in banks
 # - allow for cash input and withdrawal
 # - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
 #If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+
+
+class Client():
+    def __init__(self,name,surname,age,money,id):
+        self.name=name
+        self.surname=surname
+        self.age=age
+        self.money=money 
+        self.id=id 
+            
+
+class Bank(Client):
+    def __init__(self,badge):
+        self.badge=badge
+        self.list_of_members=[]
+
+    def add_client(self,name,surname,age,money):
+        self.list_of_members.append(Client(name,surname,age,money))    
+
+    def transfer(id1,id2,how_much):
+        if how_much<=list_of_members[id1].money:
+            list_of_members[id1].money-=how_much
+            list_of_members[id2].money+=how_much 
+        pass
+
+    def cash_input(self):
+        pass
+
+    def withdrawal(self):
+        pass
+
+if __name__=='__main__':
+
+    Merlot=Bank()
+    client1=Merlot.add_client(self,"Robert","Smith",40,20000,1)
+    client2=Merlot.add_client(self,"Anna","Kowalska",35,10000,2)
+    client1.transfer(100,client2)
+    
+
